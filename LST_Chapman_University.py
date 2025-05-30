a260563a27609bde014a886af1ff58485738186b
Justin Tang <justin.jntprules@gmail.com>
2025-05-30 01:29:59 -0700

    started the repository and added the LST_Chapman_University.py file

---
+++ LST_Chapman_University.py
@@ -0,0 +1,21 @@
+import json
+import os
+import leafmap.maplibregl as leafmap
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+
+# Can add sidebar later
+
+st.title("Land Surface Temperature of Chapman University")
+st.markdown(
+    """
+    A 3D interactive web map for visualizing the land surface temperature (°C) of Chapman University and the surrounding neighborhoods in Orange, CA
+    """
+)
+
+# Add more to the markdown section later
+# Left off at displaying the map
+# Figure out how to display layer-interact
+
