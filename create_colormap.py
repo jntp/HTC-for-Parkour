ae976f3d349f85ad4ac3834449bbc1e9cdd17cc4
Justin Tang <justin.jntprules@gmail.com>
2025-07-31 01:15:58 -0700

    updated create_colormap.py code, added new magma colormap txt, applied that to COG

---
+++ create_colormap.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib import colormaps
+
+## Functions
+
+def apply_colormap(colormap, min_value=0, max_value=1, classes=52):
+  colors = colormap(np.linspace(min_value, max_value, num=classes))
+
+  return colors # return RGBA tuples of floats
+
+def create_txt_file(filepath, colors, min_elev, max_elev, classes=52):
+  elev_values = np.linspace(min_elev, max_elev, classes)
+
+  with open(filepath, "w") as f:
+    for i, (r, g, b, _) in enumerate(colors):
+      f.write(f"{elev_values[i]} {int(r*255)} {int(g*255)} {int(b*255)}\n")
+
+## Main Code
+
+# Generate magma colormap (256 colors)
+magma = colormaps["magma"]
+fp = "data/magma_LST_colormap.txt" 
+
+# colors = magma(np.linspace(0, 1, 256)) # RGBA array
+colors = apply_colormap(magma)
+
+create_txt_file(fp, colors, 32.76, 58.87)
+
+# Write to GDAL-compatible color table file
+# with open("data/magma_colormap.txt", "w") as f:
+  # for i, (r, g, b, _) in enumerate(colors):
+    # f.write(f"{i} {int(r*255)} {int(g*255)} {int(b*255)}\n")
+
+# Check code! RGBA array does not seem correct!
