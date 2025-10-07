419c7a486a7ad92251ba9f6bb2c7ef93a9201255
Justin Tang <justin.jntprules@gmail.com>
2025-10-07 00:34:50 -0700

    test

--- pages/Land_Surface_Temperature.py
+++ pages/Land_Surface_Temperature.py
@@ -64,7 +64,7 @@ with open("temp_cog.tif", "wb") as f:
 # m.add_tile_layer(client.get_tile_url(), name="COG Layer")
 
 # Add COG
-m.add_cog_layer(url, name="Surface Temperature", opacity=0.5)
+# m.add_cog_layer(url, name="Surface Temperature", opacity=0.5)
 print(lm.cog_tile(url))  
 print(lm.cog_bands(url))
 
